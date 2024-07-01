c576460d0a8b0bd89022e01ee856efbf1c4655e4
artlu99 <alu99.sm@gmail.com>
2024-07-01 14:30:13 -0400

    endpoint

--- functions/shared/posthog.ts
+++ functions/shared/posthog.ts
@@ -1,7 +1,7 @@
 import { Env } from '../common';
 
 const sendPosthogEvent = async (env: Env, event: string, distinct_id: string) => {
-  const url = env.REACT_APP_PUBLIC_POSTHOG_HOST;
+  const url = env.REACT_APP_PUBLIC_POSTHOG_HOST + '/capture/';
   const api_key = env.REACT_APP_PUBLIC_POSTHOG_KEY;
 
   const timestamp = new Date().toISOString();
@@ -15,16 +15,13 @@ const sendPosthogEvent = async (env: Env, event: string, distinct_id: string) =>
   };
 
   try {
-    console.log('posthog attempt', JSON.stringify(payload));
-
     const response = await fetch(url, {
       method: 'POST',
       headers: headers,
       body: JSON.stringify(payload),
     });
 
-    const data = await response.json();
-    console.log('posthog success', data);
+    await response.json();
   } catch (e) {
     console.error('posthog error', e);
   }
